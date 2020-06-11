fc047da26244dd85f53643591c1ba85ab7db1128
manuelwedler <manuel.wedler@googlemail.com>
2020-06-11 09:07:19 +0200

    Wip

--- src/app/components/history-table/history-table.component.ts
+++ src/app/components/history-table/history-table.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { PaymentHistoryPollingService } from '../../services/payment-history-polling.service';
-import { Subject, combineLatest } from 'rxjs';
+import { Subject, combineLatest, Subscription } from 'rxjs';
 import { PaymentEvent } from '../../models/payment-event';
 import { AddressBookService } from '../../services/address-book.service';
 import { Animations } from '../../animations/animations';
@@ -9,7 +9,7 @@ import { UserToken } from '../../models/usertoken';
 import { SharedService } from '../../services/shared.service';
 import { matchesToken, matchesContact } from '../../shared/keyword-matcher';
 import { Contact } from '../../models/contact';
-import { takeUntil, map } from 'rxjs/operators';
+import { takeUntil, map, filter, tap, last } from 'rxjs/operators';
 import { PendingTransferPollingService } from '../../services/pending-transfer-polling.service';
 import { RaidenService } from '../../services/raiden.service';
 
@@ -25,16 +25,28 @@ export interface HistoryEvent extends PaymentEvent {
 })
 export class HistoryTableComponent implements OnInit, OnDestroy {
     private static ITEMS_PER_PAGE = 4;
+    private static EVENTS_PER_CALL = 25;
 
     @Input() showAll = false;
 
-    visibleHistory: HistoryEvent[] = [];
+    visibleHistory: HistoryEvent[] = [];//todo rename
     selectedToken: UserToken;
-    currentPage = 0;
-    numberOfPages = 0;
+    currentPage = 1;
+    onLastPage = false;
 
-    private history: HistoryEvent[] = [];
+    // private history: HistoryEvent[] = [];
     private searchFilter = '';
+    private readonly historySubject: Subject<HistoryEvent[]> = new Subject();
+    private historyPollingSubscription: Subscription;
+    private totalNumberOfEvents = 0;
+    private numberOfPendingEvents = 0;
+    private offset = 0;
+    private currentCallPage = 1;
+    private onLastCallPage = false;
+    private gettingPreviousPage = false;
+    private firstVisibleElementIndex = -1;
+    private lastVisibleElementIndex = -1;
+    private loaded = false;
     private ngUnsubscribe = new Subject();
 
     constructor(
@@ -46,55 +58,150 @@ export class HistoryTableComponent implements OnInit, OnDestroy {
         private raidenService: RaidenService
     ) {}
 
+    get numberOfPages(): number {
+        return Math.ceil(
+            (this.totalNumberOfEvents + this.numberOfPendingEvents) /
+                HistoryTableComponent.ITEMS_PER_PAGE
+        );
+    }
+
     ngOnInit() {
-        combineLatest([
-            this.paymentHistoryPollingService.getHistory(),
-            this.pendingTransferPollingService.pendingTransfers$,
-        ])
-            .pipe(
-                map(([paymentEvents, pendingTransfers]) => {
-                    const pendingEvents: HistoryEvent[] = pendingTransfers.map(
-                        (pendingTransfer) => {
-                            const initiator =
-                                pendingTransfer.role === 'initiator';
-                            const event: HistoryEvent = {
-                                target: pendingTransfer.target,
-                                initiator: pendingTransfer.initiator,
-                                event: initiator
-                                    ? 'EventPaymentSentSuccess'
-                                    : 'EventPaymentReceivedSuccess',
-                                amount: pendingTransfer.locked_amount,
-                                identifier: pendingTransfer.payment_identifier,
-                                log_time: '',
-                                token_address: pendingTransfer.token_address,
-                                pending: true,
-                            };
-                            return event;
+        this.paymentHistoryPollingService.newPaymentEvents$
+            .pipe(takeUntil(this.ngUnsubscribe))
+            .subscribe((events) => {
+                console.log('eventlength', events.length);
+                if (!this.loaded || events.length !== 0) {
+                    this.totalNumberOfEvents = this.paymentHistoryPollingService.totalNumberOfEvents;
+                    this.resetHistoryPollingParameters(); //todo i want to keep the currentPage
+                    this.updateHistoryPolling();
+                }
+                this.loaded = true;
+            });
+
+        const pendingEvents$ = this.pendingTransferPollingService.pendingTransfers$.pipe(
+            map((pendingTransfers) =>
+                pendingTransfers
+                    .map((pendingTransfer) => {
+                        const initiator = pendingTransfer.role === 'initiator';
+                        const event: HistoryEvent = {
+                            target: pendingTransfer.target,
+                            initiator: pendingTransfer.initiator,
+                            event: initiator
+                                ? 'EventPaymentSentSuccess'
+                                : 'EventPaymentReceivedSuccess',
+                            amount: pendingTransfer.locked_amount,
+                            identifier: pendingTransfer.payment_identifier,
+                            log_time: '',
+                            token_address: pendingTransfer.token_address,
+                            pending: true,
+                        };
+                        return event;
+                    })
+                    .reverse()
+            ) // todo maybe filter before tap
+            // tap((pendingEvents) => {
+            //     if (this.numberOfPendingEvents !== pendingEvents.length) {
+            //         this.numberOfPendingEvents = pendingEvents.length;
+            //         this.updateHistoryPolling();//todo
+            //     }
+            // }),
+            // map((pendingEvents) => {
+            //     // todo filter before slice
+            //     const start =
+            //         HistoryTableComponent.ITEMS_PER_PAGE *
+            //         (this.currentPage - 1);
+            //     const end = start + HistoryTableComponent.ITEMS_PER_PAGE;
+            //     return pendingEvents.slice(start, end);
+            // }) // todo buggy!!!!!
+        );
+
+        const history$ = this.historySubject.pipe(
+            map((events) => {
+                const displayableEvents: HistoryEvent[] = [];
+                let firstElement = -1;
+                let lastElement = -1;
+
+                if (this.gettingPreviousPage) {
+                    for (let i = 0; i < events.length; i++) {
+                        if (this.matchesFilter(events[i])) {
+                            displayableEvents.push(events[i]);
+                            firstElement = i;
+                            if (lastElement === -1) {
+                                lastElement = i;
+                            }
                         }
-                    );
-                    return paymentEvents.concat(pendingEvents);
-                }),
+                        if (
+                            displayableEvents.length ===
+                            HistoryTableComponent.ITEMS_PER_PAGE
+                        ) {
+                            break;
+                        }
+                    }
+                    displayableEvents.reverse();
+                } else {
+                    for (let i = events.length - 1; i >= 0; i--) {
+                        if (this.matchesFilter(events[i])) {
+                            displayableEvents.push(events[i]);
+                            lastElement = i;
+                            if (firstElement === -1) {
+                                firstElement = i;
+                            }
+                        }
+                        if (
+                            displayableEvents.length ===
+                            HistoryTableComponent.ITEMS_PER_PAGE
+                        ) {
+                            break;
+                        }
+                    }
+                    this.onLastPage = this.onLastCallPage && this.getFilteredEvents(events).length <= HistoryTableComponent.ITEMS_PER_PAGE;//todo problem with ret only 4, we need to poll more to know if there is more
+                }
+
+                if (
+                    displayableEvents.length ===
+                        HistoryTableComponent.ITEMS_PER_PAGE ||
+                    this.onLastPage
+                ) {
+                    this.currentCallPage = 1;
+                    this.firstVisibleElementIndex = firstElement + this.offset;
+                    this.lastVisibleElementIndex = lastElement + this.offset;
+                    console.log('firstlast', this.firstVisibleElementIndex, this.lastVisibleElementIndex)
+                    return displayableEvents;
+                }
+
+                //todo what if on last page: && currentCallPage !== totalNumberOfEvents / EVENTS_PER_CALL
+                this.currentCallPage++;//todo when reset??
+                this.updateHistoryPolling();
+                return undefined;
+            }),
+            filter((events) => events !== undefined)
+        );
+
+        combineLatest([history$, pendingEvents$])
+            .pipe(
+                map(([paymentEvents, pendingEvents]) =>
+                    pendingEvents.concat(paymentEvents)
+                ), 
                 takeUntil(this.ngUnsubscribe)
             )
             .subscribe((events) => {
-                this.history = events;
-                this.updateVisibleEvents();
+                this.visibleHistory = events;
             });
 
         this.selectedTokenService.selectedToken$
             .pipe(takeUntil(this.ngUnsubscribe))
             .subscribe((token: UserToken) => {
                 this.selectedToken = token;
-                this.currentPage = 0;
-                this.updateVisibleEvents();
+                this.resetHistoryPollingParameters();
+                this.updateHistoryPolling();
             });
 
         this.sharedService.searchFilter$
             .pipe(takeUntil(this.ngUnsubscribe))
             .subscribe((value) => {
                 this.searchFilter = value;
-                this.currentPage = 0;
-                this.updateVisibleEvents();
+                this.resetHistoryPollingParameters();
+                this.updateHistoryPolling();
             });
     }
 
@@ -108,19 +215,24 @@ export class HistoryTableComponent implements OnInit, OnDestroy {
     }
 
     nextPage() {
-        if (this.currentPage + 1 >= this.numberOfPages) {
+        if (this.onLastPage) {
             return;
         }
-        this.currentPage += 1;
-        this.updateVisibleEvents();
+        this.currentPage += 1;// todo this can only get safely set when polling done
+        this.gettingPreviousPage = false;
+        this.currentCallPage = 1;
+        this.updateHistoryPolling();
     }
 
     previousPage() {
-        if (this.currentPage <= 0) {
+        if (this.currentPage <= 1) {
             return;
         }
-        this.currentPage -= 1;
-        this.updateVisibleEvents();
+        this.currentPage -= 1;// todo this can only get safely set when polling done
+        this.onLastPage = false;
+        this.gettingPreviousPage = true;
+        this.currentCallPage = 1;
+        this.updateHistoryPolling();
     }
 
     paymentPartner(event: HistoryEvent): string {
@@ -147,36 +259,43 @@ export class HistoryTableComponent implements OnInit, OnDestroy {
         return this.raidenService.getUserToken(event.token_address);
     }
 
+    filterActive(): boolean {
+        return !!this.selectedToken || !!this.searchFilter;
+    }
+
     private addressLabel(address: string): string | undefined {
         return this.addressBookService.get()[address];
     }
 
-    private updateVisibleEvents() {
-        const filteredEvents = this.getFilteredEvents();
-        this.numberOfPages = Math.ceil(
-            filteredEvents.length / HistoryTableComponent.ITEMS_PER_PAGE
-        );
+    // private updateVisibleEvents() {
+    //     // how to get number of pages
+    //     const filteredEvents = this.getFilteredEvents();
+    //     this.numberOfPages = Math.ceil(
+    //         filteredEvents.length / HistoryTableComponent.ITEMS_PER_PAGE
+    //     );
 
-        const visibleEvents: HistoryEvent[] = [];
-        const start = this.currentPage * HistoryTableComponent.ITEMS_PER_PAGE;
-        for (let i = filteredEvents.length - 1 - start; i >= 0; i--) {
-            if (visibleEvents.length >= HistoryTableComponent.ITEMS_PER_PAGE) {
-                break;
-            }
-            visibleEvents.push(filteredEvents[i]);
-        }
-        this.visibleHistory = visibleEvents;
+    //     const visibleEvents: HistoryEvent[] = [];
+    //     const start = this.currentPage * HistoryTableComponent.ITEMS_PER_PAGE;
+    //     for (let i = filteredEvents.length - 1 - start; i >= 0; i--) {
+    //         if (visibleEvents.length >= HistoryTableComponent.ITEMS_PER_PAGE) {
+    //             break;
+    //         }
+    //         visibleEvents.push(filteredEvents[i]);
+    //     }
+    //     this.visibleHistory = visibleEvents;
+    // } // mucho todo
+
+    private getFilteredEvents(events: HistoryEvent[]): HistoryEvent[] {
+        return events.filter((event) => this.matchesFilter(event));
     }
 
-    private getFilteredEvents() {
-        return this.history.filter(
-            (event) =>
-                event.event !== 'EventPaymentSentFailed' &&
-                this.matchesSearchFilter(event) &&
-                !(
-                    this.selectedToken &&
-                    event.token_address !== this.selectedToken.address
-                )
+    private matchesFilter(event: HistoryEvent): boolean {
+        return (
+            this.matchesSearchFilter(event) &&
+            !(
+                this.selectedToken &&
+                event.token_address !== this.selectedToken.address
+            )
         );
     }
 
@@ -209,5 +328,46 @@ export class HistoryTableComponent implements OnInit, OnDestroy {
             matchingToken ||
             matchingContact
         );
+    }
+
+    private updateHistoryPolling() {
+        if (this.gettingPreviousPage) {
+            this.offset = this.firstVisibleElementIndex + 1;
+        } else {
+            this.offset =
+                this.lastVisibleElementIndex -
+                this.currentCallPage * HistoryTableComponent.EVENTS_PER_CALL;
+        }
+
+        // todo this.offset filtered pending transfers
+        let limit =
+            this.currentCallPage * HistoryTableComponent.EVENTS_PER_CALL;
+        if (this.offset <= 0) {
+            limit += this.offset;
+            this.offset = 0;
+            this.onLastCallPage = true;// todo bug!
+        }
+        console.log('pollpage', limit, this.offset);
+
+        if (this.historyPollingSubscription) {
+            this.historyPollingSubscription.unsubscribe();
+        }
+        this.historyPollingSubscription = this.paymentHistoryPollingService
+            .getHistory(
+                undefined,
+                undefined,
+                Math.max(0, limit),
+                this.offset
+            )
+            .pipe(takeUntil(this.ngUnsubscribe))
+            .subscribe(this.historySubject);
+    }
+
+    private resetHistoryPollingParameters() {
+        this.currentPage = 1;
+        this.lastVisibleElementIndex = this.totalNumberOfEvents;
+        this.gettingPreviousPage = false;
+        this.currentCallPage = 1;
+        this.onLastCallPage = false;
     }
 }
